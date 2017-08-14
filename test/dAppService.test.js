1da0928f19661e08d895a969cd0d972cc4ce28d2
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-08-15 01:06:37 +0800

    Add sendRawTransaction

--- test/dAppService.test.js
+++ test/dAppService.test.js
@@ -4,15 +4,17 @@ const Tx = require('ethereumjs-tx');
 
 web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io' + process.env.INFURA_API_KEY));
 
-export const getBlockNumber = () => {
-  return web3.eth.blockNumber;
+console.log(process.env.PRIVATE_KRY);
+
+let payload = {
+  from: "0x7c20badacd20f09f972013008b5e5dae82670c8d",
+  to: "0xd6026ddc3a2be02a3577de714a98e24dc4a89dbf",
+  value: "0x100",
+  data: "",
+  privateKey: process.env.PRIVATE_KRY
 };
 
-export const getAddressBalanceLoad = (address) => {
-  return web3.fromWei(web3.eth.getBalance(address), "ether").toString(10);
-};
-
-export const sendRawTransaction = (payload) => {
+function sendRawTransaction(payload) {
   let privateKey = new Buffer(payload.privateKey, 'hex');
   let rawTx = {
     nonce: '0x' + web3.eth.getTransactionCount(payload.from).toString(16),
@@ -28,22 +30,14 @@ export const sendRawTransaction = (payload) => {
   tx.sign(privateKey);
 
   let serializedTx = tx.serialize();
+  console.log(serializedTx.toString('hex'));
 
-  // TODO async await for transactionHash
   web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'), function(err, hash) {
     if (!err)
-      console.log("transactionHash", hash); // TODO listen transactionReceipt
+      console.log(hash); // TODO listen transactionReceipt
     else 
       console.log("err", err);
   });
-};
+}
 
-export const getEstimateGas = (payload) => {
-  var result = web3.eth.estimateGas({
-    from: payload.from,
-    to: payload.to, 
-    value: payload.value,
-    data: payload.data
-  });
-  return result;
-};
+sendRawTransaction(payload);
