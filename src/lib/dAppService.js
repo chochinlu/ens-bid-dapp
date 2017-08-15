1b4c3018272da94339f4869893cece5d26ef48c7
Lu Cho-Ching <choching.work@gmail.com>
2017-08-15 15:42:01 +0800

    Use babel to make service import workable & misc fix

--- src/lib/dAppService.js
+++ src/lib/dAppService.js
@@ -2,48 +2,52 @@ const Web3 = require('web3');
 const web3 = new Web3();
 const Tx = require('ethereumjs-tx');
 
-web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io' + process.env.INFURA_API_KEY));
-
-export const getBlockNumber = () => {
-  return web3.eth.blockNumber;
+const setWeb3Provider = () => {
+  const API_KEY = process.env.INFURA_API_KEY; //TODO: should check if is empty string
+  console.log(``)
+  const provider = `https://ropsten.infura.io/${API_KEY}`;
+  web3.setProvider(new web3.providers.HttpProvider(provider));
 };
 
-export const getAddressBalanceLoad = (address) => {
-  return web3.fromWei(web3.eth.getBalance(address), "ether").toString(10);
+setWeb3Provider();
+
+export const getBlockNumber = () => web3.eth.blockNumber;
+
+export const getAddressBalanceLoad = address => {
+  return web3.fromWei(web3.eth.getBalance(address), 'ether').toString(10);
 };
 
-export const sendRawTransaction = (payload) => {
-  let privateKey = new Buffer(payload.privateKey, 'hex');
-  let rawTx = {
-    nonce: '0x' + web3.eth.getTransactionCount(payload.from).toString(16),
+export const sendRawTransaction = payload => {
+  const {privateKey, from, to, value, data} = payload;
+
+  const hexPivateKey = new Buffer(privateKey, 'hex');
+
+  const rawTx = {
+    nonce: '0x' + web3.eth.getTransactionCount(from).toString(16),
     gasPrice: web3.eth.gasPrice, 
-    from: payload.from,
-    to: payload.to, 
-    value: payload.value, 
-    data: payload.data
-  }
-  const estimateGas = web3.eth.estimateGas(rawTx);
-  rawTx.gasLimit = estimateGas;
-  let tx = new Tx(rawTx);
-  tx.sign(privateKey);
+    from,
+    to, 
+    value, 
+    data
+  };
 
-  let serializedTx = tx.serialize();
+  rawTx.gasLimit = web3.eth.estimateGas(rawTx);
+  
+  const tx = new Tx(rawTx);
+  tx.sign(hexPivateKey);
 
-  // TODO async await for transactionHash
-  web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'), function(err, hash) {
+  const serializedTx = tx.serialize();
+
+  web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), (err, hash) => {
     if (!err)
-      console.log("transactionHash", hash); // TODO listen transactionReceipt
+      console.log(`transactionHash: ${hash}`); // TODO listen transactionReceipt
     else 
-      console.log("err", err);
+      console.log(`err: ${err}`);
   });
 };
+// module.exports.sendRawTransaction = sendRawTransaction;
 
-export const getEstimateGas = (payload) => {
-  var result = web3.eth.estimateGas({
-    from: payload.from,
-    to: payload.to, 
-    value: payload.value,
-    data: payload.data
-  });
-  return result;
+export const getEstimateGas = payload => {
+  const {from, to, value, data} = payload;
+  return web3.eth.estimateGas({ from, to, value, data });
 };
--- test/dAppService.test.js
+++ test/dAppService.test.js
@@ -1,43 +1,14 @@
-const Web3 = require('web3');
-const web3 = new Web3();
-const Tx = require('ethereumjs-tx');
+import {sendRawTransaction} from './dAppService';
 
-web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io' + process.env.INFURA_API_KEY));
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+console.log(`PRIVATE_KEY: ${PRIVATE_KEY}`);
 
-console.log(process.env.PRIVATE_KRY);
-
-let payload = {
-  from: "0x7c20badacd20f09f972013008b5e5dae82670c8d",
-  to: "0xd6026ddc3a2be02a3577de714a98e24dc4a89dbf",
-  value: "0x100",
-  data: "",
-  privateKey: process.env.PRIVATE_KRY
+const testPayload = {
+  from: '0x7c20badacd20f09f972013008b5e5dae82670c8d',
+  to: '0xd6026ddc3a2be02a3577de714a98e24dc4a89dbf',
+  value: '0x100',
+  data: '',
+  privateKey: PRIVATE_KEY
 };
 
-function sendRawTransaction(payload) {
-  let privateKey = new Buffer(payload.privateKey, 'hex');
-  let rawTx = {
-    nonce: '0x' + web3.eth.getTransactionCount(payload.from).toString(16),
-    gasPrice: web3.eth.gasPrice, 
-    from: payload.from,
-    to: payload.to, 
-    value: payload.value, 
-    data: payload.data
-  }
-  const estimateGas = web3.eth.estimateGas(rawTx);
-  rawTx.gasLimit = estimateGas;
-  let tx = new Tx(rawTx);
-  tx.sign(privateKey);
-
-  let serializedTx = tx.serialize();
-  console.log(serializedTx.toString('hex'));
-
-  web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'), function(err, hash) {
-    if (!err)
-      console.log(hash); // TODO listen transactionReceipt
-    else 
-      console.log("err", err);
-  });
-}
-
-sendRawTransaction(payload);
+sendRawTransaction(testPayload);
