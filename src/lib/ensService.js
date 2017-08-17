bebb2a59704bd653246722ebb0ed768e53c87f7f
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-08-17 23:46:17 +0800

    Update package version

--- src/lib/ensService.js
+++ src/lib/ensService.js
@@ -17,30 +17,43 @@ const setWeb3Provider = () => {
 
 setWeb3Provider();
 
-export const searchAddress = (address) => {
-  ens = new ENS(web3);
-  ens.resolver(address).addr().then(function(addr) { 
-    console.log(addr);
-  }).catch(function(err) {
-    console.log("ENS name not found or unavailable");
+// https://www.npmjs.com/package/eth-registrar-ens
+// https://github.com/ethereum/ens-registrar-dapp/blob/master/app/imports/lib/ethereum.js
+const initRegistrar = () => {
+  /*return new Promise((resolve, reject) => {
+    try {
+      ens = new ENS(web3, ENS_ADDRESS);
+      registrar = new Registrar(web3, ens, 'eth', 7, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        //TODO: Check that the registrar is correctly instanciated
+        console.log('done initializing', err, result)
+        resolve();
+      });
+    } catch(e) {
+      reject('Error initialiting ENS registrar: ' + e);
+    }
+  });*/
+  ens = new ENS(web3, ENS_ADDRESS);
+  registrar = new Registrar(web3, ens, 'eth', 7, (err, result) => {
+    if (err) {
+      console.log("err", err);
+    }
+    //TODO: Check that the registrar is correctly instanciated
+    console.log('done initializing', err, result)
   });
 }
 
-// https://www.npmjs.com/package/eth-registrar-ens
-// https://github.com/ethereum/ens-registrar-dapp/blob/master/app/imports/lib/ethereum.js
-export const initRegistrar = async () => {
-  try {
-    ens = new ENS(web3);
-    
-    registrar = await new Registrar(web3, ens, 'eth', 7, function(err, result) {
-      //TODO: Check that the registrar is correctly instanciated
-      console.log('done initializing', err, result);
-    });
-    return registrar;
-    //console.log(registrar);
-    //console.log(registrar.contract);
-    //const result = registrar.startAuction(namehash(name), { from: '0x7c20badacd20f09f972013008b5e5dae82670c8d', gas: 4700000 });
-  } catch(error) {
-    console.log(`ENS init registrar error: ${error}`);
-  }
+initRegistrar();
+
+// 要降版本才可以 work
+export const searchAddress = (address) => {
+  ens = new ENS(web3, ENS_ADDRESS);
+  //console.log("resolver", ens.resolver);
+  console.log(ens.resolver(address).addr());/*.then(function(addr) { 
+    console.log(addr);
+  }).catch(function(err) {
+    console.log("ENS name not found or unavailable");
+  });*/
 }
