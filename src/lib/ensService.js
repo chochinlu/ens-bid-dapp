7c970c748f987e870849a6dbdd859530fb620251
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-08-20 00:21:02 +0800

    Fix newBid for payable ether value

--- src/lib/ensService.js
+++ src/lib/ensService.js
@@ -88,7 +88,7 @@ export const newBid = (name, ether, secret, privateKey) => {
   const payload = {
     from: fromAddress,
     to: ethRegistrarAddress,
-    value: '0x0',
+    value: web3.toHex(web3.toWei(ether, "ether")),
     data: byteData,
     privateKey: privateKey
   };
