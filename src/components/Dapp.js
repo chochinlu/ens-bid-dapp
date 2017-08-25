4d64b9c0d2ca1cc406e7c4d6577c8edeee3e99a5
laiyanlong <laiyanlong@gmail.com>
2017-08-25 15:41:21 +0800

    add dappBar and update ensCard component

--- src/components/Dapp.js
+++ src/components/Dapp.js
@@ -3,12 +3,14 @@ import BlockNumber from './BlockNumber';
 import AddressSearch from './AddressSearch';
 import {JsonKeyUploader} from './JsonKeyUploader'; 
 import SearchBar from './SearchBar';
+import EnsCard from './EnsCard';
 
 export const Dapp = () => (
   <div>
     <BlockNumber />
     <AddressSearch />
     <SearchBar />
+    <EnsCard />
     <JsonKeyUploader />
   </div>
 );
--- src/components/EnsCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EnsCard = () => (
-  <div>
-    TODO: ENS CARD
-  </div>
-)
-
-export default EnsCard;
