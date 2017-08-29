f2e877207a3613dbcc3655bc4e7f135fecebb1c3
Lu Cho-Ching <choching.work@gmail.com>
2017-08-29 14:30:21 +0800

    Add fetching current network

--- src/components/MetaMaskWallet/MetaMaskWallet.js
+++ src/components/MetaMaskWallet/MetaMaskWallet.js
@@ -4,7 +4,8 @@ import './MetaMaskWallet.css'
 const messages = {
   'LOAD_MATAMASK_WALLET_ERROR': 'Load metamask wallet error, maybe try Metamask later, or upload a wallet json file.',
   'EMPTY_METAMASK_ACCOUNT': 'You can choose one MetaMask wallet by unlocking it',
-  'METAMASK_ACCOUNT': 'You have choosen the MetamMask Wallet: '
+  'METAMASK_ACCOUNT': 'You have choosen the MetamMask Wallet: ',
+  'NETWORK_ERROR': 'Network error, please check it.'
 };
 
 export class MetaMaskWallet extends Component {
@@ -12,7 +13,8 @@ export class MetaMaskWallet extends Component {
     super(props);
     this.state = {
       message: '',
-      account: ''
+      account: '',
+      networkId: null
     };
   }
 
@@ -29,8 +31,34 @@ export class MetaMaskWallet extends Component {
     });
   }
 
+  fetchNetwork() {
+    const { web3 } = window;
+
+    web3 && web3.version && web3.version.getNetwork((err, netId) => {
+      if (err) {
+        this.setState({message: messages.NETWORK_ERROR});
+      } else {
+        this.setState({ networkId: netId })
+      }
+    });
+  }
+
+  getNetworkName(networkId) {
+    switch (networkId) {
+      case '1':
+        return 'MAINNET';
+      case '2':
+        return 'MORDEN';
+      case '3':
+        return 'ROPSTEN';
+      default:
+        return 'UNKNOWN';
+    }
+  }
+
   componentDidMount() {
     this.fetchAccounts();
+    this.fetchNetwork();
   }
 
   render() {
@@ -38,10 +66,14 @@ export class MetaMaskWallet extends Component {
     const accountInfo = this.state.account !== '' &&
       <p>{messages.METAMASK_ACCOUNT + this.state.account}</p>
 
+    const currentNetwork = this.state.networkId &&
+      <p>Current Network: {this.getNetworkName(this.state.networkId)}</p>
+
     return (
       <div className='MetaMaskWallet'>
         <h1>MetaMask Wallet</h1>
         {accountInfo}
+        {currentNetwork}
         <p>{this.state.message}</p>
       </div>
     );
