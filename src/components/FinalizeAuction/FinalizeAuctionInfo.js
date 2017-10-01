9406f1ca7d2c8401f1497e6213eabc45021af12f
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-10-01 16:05:53 +0800

    Add Finalize Auction Page

---
+++ src/components/FinalizeAuction/FinalizeAuctionInfo.js
@@ -0,0 +1,18 @@
+// @flow weak
+
+import React from 'react';
+import './FinalizeAuctionIfn.css';
+
+export const FinalizeAuctionInfo = () => (
+  // TODO switch FinalizeAuctionInfo when Success
+  <div>
+    <h2>mybidens.eth</h2>
+    <p>
+      Congratulations! You've owned the mybidens.eth domain name!
+    </p>
+    <div>
+      <button>Back to Search</button>
+      <button>My ENS List</button>
+    </div>
+  </div>
+);
