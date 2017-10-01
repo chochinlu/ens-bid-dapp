da13d479061cb3f02801d16a74c7cb5e1898e7fd
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-10-01 15:45:18 +0800

    Add RevealAuction Page

---
+++ src/components/RevealAuction/RevealAuction.js
@@ -0,0 +1,16 @@
+// @flow weak
+
+import React from 'react';
+import {RevealAuctionForm} from './RevealAuctionForm';
+//import AuctionStepper from './AuctionStepper';
+import './RevealAuction.css';
+
+export const RevealAuction = () => (
+  // TODO 超過時間才能揭露
+  // TODO 加上 AuctionStepper
+  // TODO Timeline 顯示
+  <div>
+    
+    <RevealAuctionForm />
+  </div>
+);
---
+++ src/components/RevealAuction/RevealAuctionInfo.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './RevealAuctionInfo.css';
+
+export const RevealAuctionInfo = () => (
+  <div>
+    <h2>mybidens.eth</h2>
+    <div>
+      Timeline
+    </div>
+    <div>
+      <div>
+        <div>Email: </div>
+        <div>
+          youremail@example.com
+        </div>
+      </div>
+      <div>
+        <div>ETH: </div>
+        <div>
+          0.01
+        </div>
+      </div>
+      <div>
+        <div>TxHash: </div>
+        <div>
+          0x0
+        </div>
+      </div>
+    </div>
+    <div>
+      <button>Back to Search</button>
+      <button>My ENS List</button>
+    </div>
+  </div>
+);
