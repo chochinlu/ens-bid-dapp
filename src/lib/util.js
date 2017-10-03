65d2c3c19e115ba0fb7f339610970ed230332b8c
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-03 23:23:55 +0800

    for get transaction hash url params use function

--- src/lib/util.js
+++ src/lib/util.js
@@ -12,3 +12,17 @@ export const Box = x => ({
   fold: f => f(x),
   inspect: () => `Box(${x})`
 });
+
+// if search params not exists return false
+export const urlQueryParamsObject = () => {
+  let urlSearchString = window.location.search;
+  if (urlSearchString.indexOf('?') == -1) return false;
+  urlSearchString = urlSearchString.split('?')[1];
+  const query = urlSearchString
+    .split('&')
+    .map(q => q.split('='))
+    .filter(([name, value]) => name && value)
+    .map(([name, value]) => {name: decodeURI(value)})
+    .reduce((a, b) => Object.assign(a,b), {});
+  return query;
+}
