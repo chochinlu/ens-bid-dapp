ff250642979012c67c7134f1b2eeafeb4ec997ad
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-10-07 23:09:12 +0800

    Add Drawer Component for Menu

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -6,7 +6,15 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import { blueGrey } from 'material-ui/colors';
+import MenuIcon from 'material-ui-icons/Menu';
 import {WalletDialog} from './WalletDialog';
+import IconButton from 'material-ui/IconButton';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Drawer from 'material-ui/Drawer';
+import ChevronLeftIcon from 'material-ui-icons/ChevronLeft';
+import HomeIcon from 'material-ui-icons/Home';
+import classNames from 'classnames';
 import {Title} from './Title';
 import {Wallet} from './Wallet';
 import './Top.css';
@@ -18,15 +26,27 @@ const styles = {
   flex: {
     flex: 1
   },
+  hide: {
+    display: 'none',
+  },
 };
 
 class Top extends Component {
   state = {
     open: false,
+    menu: false,
   };
 
   handleRequestClose = () => {
     this.setState({ open: false });
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ menu: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ menu: false });
   };
 
   render () {
@@ -36,6 +56,13 @@ class Top extends Component {
       <div className={classes.root}>
         <AppBar position="static" style={{ backgroundColor: blueGrey[900] }}>
           <Toolbar>
+            <IconButton
+              color="contrast"
+              aria-label="open menu"
+              onClick={this.handleDrawerOpen}
+              className="MenuBtn">  
+              <MenuIcon />
+            </IconButton>
             <Title className={classes.flex} />
             <Wallet 
               privateKey={this.props.privateKey}
@@ -47,6 +74,59 @@ class Top extends Component {
               />
           </Toolbar>
         </AppBar>
+        <Drawer
+          type="persistent"
+          open={this.state.menu}>
+          <div className="Top-Drawer">
+            <div className="Top-Drawer-Chevron">
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                  <i className="material-icons">home</i>
+                </ListItemIcon>
+                <ListItemText primary="ENS.BID" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                  <i className="material-icons">view_list</i>
+                </ListItemIcon>
+                <ListItemText primary="My ENS List" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                  <i className="material-icons">gavel</i>
+                </ListItemIcon>
+                <ListItemText primary="ENS Trade" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                  <i className="material-icons">attach_money</i>
+                </ListItemIcon>
+                <ListItemText primary="ENS Loan" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                <i className="material-icons">help_outline</i>
+                </ListItemIcon>
+                <ListItemText primary="FAQ" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+              <ListItem button className="Top-Drawer-ListItem">
+                <ListItemIcon>
+                  <i className="material-icons">assignment</i>
+                </ListItemIcon>
+                <ListItemText primary="Terms & Service" className="Top-Drawer-ListItemText"/>
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
       </div>
     );
   }
--- src/components/Top/Wallet.js
+++ src/components/Top/Wallet.js
@@ -5,6 +5,9 @@ import './Wallet.css';
 export const Wallet = (props) => {
   const unlockWallet = props.privateKey && "unlock";
   return (
-    <Button className={unlockWallet} color="contrast" onClick={props.onClick}>Wallet</Button>
+    <Button className={unlockWallet} color="contrast" onClick={props.onClick}>
+      <i className="material-icons">account_balance_wallet</i>
+      <div className="WalletText">Wallet</div>
+    </Button>
   );
 };
