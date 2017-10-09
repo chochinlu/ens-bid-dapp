827378226e026538e4323cd3e01d0a8da34ac56c
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-10-09 23:21:26 +0800

    Update Footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import { blueGrey } from 'material-ui/colors';
 import './Footer.css';
 
 class Footer extends Component {
   render () {
+    const classes = this.props.classes;
+    
     return (
-      <footer>
-        © Copyright 2017 ENS.BID - All Rights Reserved
-      </footer>
+      <AppBar position="static" style={{ backgroundColor: blueGrey[900] }} className="Footer">
+        <div>
+          © Copyright 2017 ENS.BID - All Rights Reserved
+        </div>
+      </AppBar>
     )
   }
 };
