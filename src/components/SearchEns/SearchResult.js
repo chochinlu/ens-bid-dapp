0c2d3a46cf845e2a0400f655dfb9dc13b8d9c2f1
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-10-14 23:11:31 +0800

    Update

--- src/components/SearchEns/SearchResult.js
+++ src/components/SearchEns/SearchResult.js
@@ -9,7 +9,8 @@ class SearchResultItem extends Component {
     if ((this.props.searchResult.searchName).length < 7) return alert('should greater than 7 words');
     
     // validate user login
-    //if (!(this.props.address && this.props.privateKey)) return alert('should login first');
+    // TODO move this check before Auction
+    if (!(this.props.address && this.props.privateKey)) return alert('should login first');
 
     let step = '';
     switch (this.props.searchResult.state) {
