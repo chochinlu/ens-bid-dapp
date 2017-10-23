5c0af742888d6b626a8ef8e61e55593946e9c457
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-23 16:09:15 +0800

    minor alter util code

--- src/lib/util.js
+++ src/lib/util.js
@@ -35,5 +35,4 @@ export const urlQueryParamsObject = (url) => {
 // time formate related methods
 export const checkBeforeNow = (time) => format(moment()).diff(format(time)) < 0;
 export const format = (date) => moment(date, 'dddd, MMMM D YYYY, h:mm:ss a z');
-
 export const momentFromNow = (thisTime) => moment(thisTime).fromNow();
