4d19602a917525a6a5457507202fbb844419c0fa
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-25 15:25:35 +0800

    update finalize ens service methods to new version

--- src/components/FinalizeAuction/FinalizeAuction.js
+++ src/components/FinalizeAuction/FinalizeAuction.js
@@ -1,8 +1,26 @@
 // @flow weak
 import React, {Component} from 'react';
 import {finalizeAuction} from '../../lib/ensService';
+import {sendRawTransaction} from '../../lib/dAppService';
 import {FinalizeAuctionInfo} from './FinalizeAuctionInfo';
 import './FinalizeAuction.css';
+
+const handleFinalizeAuctionProcess = async (inputObj) => {
+  const {domainName, privateKey, gas} = inputObj;
+  let returnObj = {
+    txHash: '',
+    errMsg: undefined
+  }
+
+  const payload = finalizeAuction(domainName, privateKey, gas);
+  try {
+    returnObj.txHash = await sendRawTransaction(payload);
+  } catch (error) {
+    returnObj.errMsg = `finalize auction error : ${error}`;
+  }
+
+  return returnObj;
+}
 
 const FinalizeAuctionForm = (props) => (
   <div>
@@ -40,16 +58,28 @@ export class FinalizeAuction extends Component {
     this.state = {
       email: '',
       finalFormSent: '',
-      gas: '21'
+      gas: '21',
+      warningOpen: false,
+      warningMessage: ''
     }
 
     this.setFinalFormSent = this.setFinalFormSent.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleWarningMessageClose = this.handleWarningMessageClose.bind(this);
+    this.handleWarningMessageOpen = this.handleWarningMessageOpen.bind(this);
   }
 
   setFinalFormSent(state) {
     this.setState({finalFormSent: state});
+  }
+
+  handleWarningMessageClose() {
+    this.setState({warningOpen: false});
+  }
+
+  handleWarningMessageOpen(msg) {
+    this.setState({warningOpen: true, warningMessage: msg});
   }
 
   handleChange(event) {
@@ -59,12 +89,25 @@ export class FinalizeAuction extends Component {
   // only success scenario
   handleFormSubmit(event) {
     event.preventDefault();
-    const privateKey = this.props.privateKey;
-    finalizeAuction(this.props.searchResult.searchName, privateKey, this.state.gas);
-    // TODO
-    //   if success turn to info component
-    //   if not show error
-    this.setFinalFormSent('sent');
+
+    if (!(this.props.address && this.props.privateKey)) {
+      this.handleWarningMessageOpen('Please unlock wallet before bid ENS.');
+      return;
+    }
+
+    const inputObj = {
+      domainName: this.props.searchResult.searchName,
+      privateKey: this.props.privateKey,
+      gas: this.state.gas
+    }
+
+    handleFinalizeAuctionProcess(inputObj).then((result) => {
+      // TODO
+      // not yet refactoring error message
+      (result.errMsg === undefined) ?
+        this.setFinalFormSent('sent') :
+        this.handleWarningMessageOpen(result.errMsg);
+    });
   }
 
   finalizeAuctionInfo = () => (
