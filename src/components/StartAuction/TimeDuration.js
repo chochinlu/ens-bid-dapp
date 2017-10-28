d876667480e2e988621ae2d93170109d0c1eb3ea
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-28 19:40:45 +0800

    fix finalize time duration style

--- src/components/StartAuction/TimeDuration.js
+++ src/components/StartAuction/TimeDuration.js
@@ -3,13 +3,13 @@ import {momentFromNow} from '../../lib/util';
 import './TimeDuration.css';
 
 export const TimeDuration = (props) => (
-  <div className='TimeDuration'>
-    <div className='TimeDuration-Reveal'>
+  <div className='StartAuctionTimeDuration'>
+    <div className='StartAuctionTimeDuration-Reveal'>
       <div>Reveal Bids On</div>
       <h3>{props.unsealStartsAt.toString()}</h3>
       <div>{momentFromNow(props.unsealStartsAt).toString()}</div>
     </div>
-    <div className='TimeDuration-Finalize'>
+    <div className='StartAuctionTimeDuration-Finalize'>
       <div>Auction Finalizes On</div>
       <h3>{props.registratesAt.toString()}</h3>
       <div>{momentFromNow(props.registratesAt).toString()}</div>
