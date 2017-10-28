c7a3de2c1caffa8428f68e19ea34b751973691fc
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-28 19:39:00 +0800

    fix finalize time duration style

--- src/components/FinalizeAuction/TimeDuration.js
+++ src/components/FinalizeAuction/TimeDuration.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './TimeDuration.css';
 
 export const TimeDuration = (props) => (
-  <div className='TimeDuration'>
+  <div className='Finalize-TimeDuration'>
     <div>Finalize Auction On</div>
     <h3>{props.registratesAt.toString()}</h3>
     <div>Finalization</div>
