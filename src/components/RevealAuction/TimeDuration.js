55f3c611d5b318301124ae018b437e8972b9f9cc
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-28 19:40:31 +0800

    fix reveal time duration style

--- src/components/RevealAuction/TimeDuration.js
+++ src/components/RevealAuction/TimeDuration.js
@@ -3,13 +3,13 @@ import {momentFromNow} from '../../lib/util';
 import './TimeDuration.css';
 
 export const TimeDuration = (props) => (
-  <div className='TimeDuration'>
-    <div className='TimeDuration-Reveal'>
+  <div className='RevealAuctionTimeDuration'>
+    <div className='RevealAuctionTimeDuration-Reveal'>
       <div>Reveal Auction On</div>
       <h3>{props.unsealStartsAt.toString()}</h3>
       <div>{momentFromNow(props.unsealStartsAt).toString()}</div>
     </div>
-    <div className='TimeDuration-Finalize'>
+    <div className='RevealAuctionTimeDuration-Finalize'>
       <div>Finalize Auction On</div>
       <h3>{props.registratesAt.toString()}</h3>
       <div>{momentFromNow(props.registratesAt).toString()}</div>
