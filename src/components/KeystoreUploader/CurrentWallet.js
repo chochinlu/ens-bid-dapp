596851f011be9f965dc4d817f3cb7c5742878093
Lu Cho-Ching <choching.work@gmail.com>
2017-10-30 14:55:36 +0800

    Make mobile/desktop address view correctly.

--- src/components/KeystoreUploader/CurrentWallet.js
+++ src/components/KeystoreUploader/CurrentWallet.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {getAddressBalance} from '../../lib/dAppService';
 
 export const CurrentWallet = (props) => {
-  return props.privateKey 
-    ? (
-      <div>
-        <h2>Current Wallet</h2>
-        <p>Address: {props.address}</p>
-        <p>Balance: {getAddressBalance(props.address)}</p>
-      </div>
-    ) : null;
+  return props.privateKey && 
+    <div className="CurrentWallet">
+      <h2>Current Wallet</h2>
+      <p className="CurrentWallet-address">
+        <span>Address:</span> 
+        <span>{props.address}</span>
+      </p>
+      <p><span>Balance:</span> {getAddressBalance(props.address)}</p>
+    </div>
 };
