9d69581a246575d7c2808a16175afe64e637fbae
Lu Cho-Ching <choching.work@gmail.com>
2017-10-30 14:08:35 +0800

    Add diabled/enabled button style.

--- src/components/KeystoreUploader/Actions.js
+++ src/components/KeystoreUploader/Actions.js
@@ -13,15 +13,20 @@ const ReUploadButton = (props) => {
   ): null;
 };
 
-const UnlockButton = (props) => (
-  <Button 
-    raised 
-    className="KeystoreUploader-button"
-    disabled={props.disabled}
-    onClick={props.unlockWallet}>
-    Unlock
-  </Button>
-);
+const UnlockButton = (props) => {
+  const disabled = props.disabled;
+  const classes = disabled ? '': 'KeystoreUploader-button';
+
+  return (
+    <Button 
+      raised 
+      className={classes}
+      disabled={disabled}
+      onClick={props.unlockWallet}>
+      Unlock
+    </Button>
+  );
+}
 
 export const Actions = (props) => (
   <div className="KeystoreUploader-button-container">
