2c18a4867b58b8a8e4d74c5d48d222f9404f8b05
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-31 15:33:17 +0800

    fix finalize auction info style

--- src/components/FinalizeAuction/FinalizeAuctionInfo.js
+++ src/components/FinalizeAuction/FinalizeAuctionInfo.js
@@ -1,26 +1,23 @@
 // @flow weak
 import React from 'react';
 import Button from 'material-ui/Button';
-import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Card from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import './FinalizeAuctionInfo.css';
 
 export const FinalizeAuctionInfo = (props) => (
-  // TODO switch FinalizeAuctionInfo when Success
   <Card raised>
-    <CardContent>
-      <Typography type="title" component="div">
-        ENS: {props.searchName}.eth
-      </Typography>
-      <Typography type="title" component="div">
-        Congratulations! You've owned the {props.searchName}.eth domain name!
-      </Typography>
-      <CardActions>
-        <Button raised onClick={() => props.switchPage('main')}>
+    <div className='FinalizeAuctionInfo'>
+      <h3>ENS: {props.searchName}.eth</h3>
+      <p>
+        Congratulations! You've owned the <strong>{props.searchName}.eth</strong> domain name!
+      </p>
+      <div className='FinalizeAuctionInfo-actions'>
+        <Button raised onClick={() => props.backToSearch()}>
           BACK TO SEARCH
         </Button>
         <Button raised>MY ENS LIST</Button>
-      </CardActions>
-    </CardContent>
+      </div>
+    </div>
   </Card>
 );
