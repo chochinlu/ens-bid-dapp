8a72f9dd2bc1edcd65025b7bc0485376b6fb8e0d
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-31 13:47:47 +0800

    fix start auction info component and style

--- src/components/StartAuction/StartAuctionInfo.js
+++ src/components/StartAuction/StartAuctionInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {momentFromNow} from '../../lib/util';
+import {shortFormatHash, momentFromNow} from '../../lib/util';
 import Button from 'material-ui/Button';
 import Card from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
@@ -17,6 +17,7 @@ const InfoItem = (props) => {
   let classes = '';
   if (props.title === 'ENS') classes = 'eth-item';
   if (props.title === 'TxHash') classes = 'eth-txhash';
+  if (props.title === 'JSON') classes = 'eth-json';
 
   return (
     <div className="StartAuctionInfo-info-item">
@@ -25,36 +26,33 @@ const InfoItem = (props) => {
         <span className={classes}>{props.children}</span>
       </p>
       <Divider />
-  </div>
+    </div>
   );
 };
 
-const shorter = (txHash) => 
-  `${txHash.substring(0,12)}...${txHash.substring(txHash.length -12)}`;
-
 export const StartAuctionInfo = (props) => {
   const endsMomentFromNow = momentFromNow(props.unsealStartsAt);
-  const hidden = props.registratesAt.year() === 1970;
+  const hidden = (props.registratesAt.year() - 1970) <= 0 ;
   const ethersacnUrl = process.env.REACT_APP_ETHERSCAN_URL || 'https://ropsten.etherscan.io/tx/';
   const txHashUrl = ethersacnUrl + props.auctionTXHash;
 
-  const revealAuctionOn =  !props.hidden && 
+  const revealAuctionOn = !props.hidden && 
     <RevealAuctionOn 
-      hidden={hidden}
       unsealStartsAt={props.unsealStartsAt.toString()}
       endsMomentFromNow={endsMomentFromNow.toString()}
     />;
 
   const {ethBid, ethMask, secret} = props.formResult;
 
-  const shorterTxHash = shorter(props.auctionTXHash);
+  const shorterTxHash = shortFormatHash(props.auctionTXHash);
 
   const itemTitleValue = [
     {title: 'ENS', value: `${props.searchResult.searchName}.eth`},
     {title: 'ETH Bid', value: ethBid},
     {title: 'ETH Mask', value: ethMask},
     {title: 'Secret', value: secret},
-    {title: 'TxHash', value: <a target='_blank' href={txHashUrl}>{shorterTxHash}</a>}
+    {title: 'TxHash', value: <a target='_blank' href={txHashUrl}>{shorterTxHash}</a>},
+    {title: 'JSON', value: JSON.parse(props.exportJson)}
   ];
 
   const infoItems = itemTitleValue.map(({title, value}, index) =>
