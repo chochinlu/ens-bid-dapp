d5e89069706ec4ca03a0e5f61ed3926a0ffa8cfe
metatrontu@gmail.com <metatrontu@gmail.com>
2017-10-31 13:48:05 +0800

    fix reveal auction info component and style

--- src/components/RevealAuction/RevealAuctionInfo.js
+++ src/components/RevealAuction/RevealAuctionInfo.js
@@ -1,51 +1,73 @@
 import React from 'react';
-import {momentFromNow} from '../../lib/util';
+import {shortFormatHash, momentFromNow} from '../../lib/util';
 import Button from 'material-ui/Button';
-import Card, { CardActions, CardContent } from 'material-ui/Card';
-import Typography from 'material-ui/Typography';
+import Card from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
 import './RevealAuctionInfo.css';
+
+const FinalizeAuctionOn = (props) =>
+  <div className='FinalizeAuctionOn'>
+    <h4>Finalize Auction On:</h4>
+    <p>{props.registratesAt}</p>
+    <p>{props.endsMomentFromNow}</p>
+    <Divider />
+  </div>;
+
+const InfoItem = (props) => {
+  let classes = '';
+  if (props.title === 'ENS') classes = 'eth-item';
+  if (props.title === 'TxHash') classes = 'eth-txhash';
+  if (props.title === 'JSON') classes = 'eth-json';
+
+  return (
+    <div className='RevealAuctionInfo-info-item'>
+      <p>
+        <span>{props.title}:</span>
+        <span className={classes}>{props.children}</span>
+      </p>
+      <Divider />
+    </div>
+  )
+}
 
 export const RevealAuctionInfo = (props) => {
   const endsMomentFromNow = momentFromNow(props.registratesAt);
-  const hidden = props.registratesAt.year() === 1970;
+  const hidden = (props.registratesAt.year() - 1970) <= 0;
   const ethersacnUrl = process.env.REACT_APP_ETHERSCAN_URL || 'https://ropsten.etherscan.io/tx/';
   const txHashUrl = ethersacnUrl + props.revealTXHash;
 
+  const finalizeAuctionOn = !props.hidden &&
+    <FinalizeAuctionOn
+      registratesAt={props.registratesAt.toString()}
+      endsMomentFromNow={endsMomentFromNow.toString()}
+    />;
+
+  const {ethBid, secret} = props.formResult
+
+  const shorterTxHash = shortFormatHash(props.revealTXHash);
+
+  const itemTitleValue = [
+    {title: 'ENS',      value: `${props.searchResult.searchName}.eth`},
+    {title: 'ETH Bid',  value: ethBid},
+    {title: 'Secret',   value: secret},
+    {title: 'TxHash',   value: <a target='_blank' href={txHashUrl}>{shorterTxHash}</a>},
+    {title: 'JSON',     value: JSON.parse(props.exportJson)}
+  ];
+
+  const infoItems = itemTitleValue.map(({title, value}, index) => 
+    <InfoItem key={`infoItem-${index}`} title={title}>{value}</InfoItem>
+  );
+
   return (
-    <Card raised className="RevealAuctionInfo">
-      <CardContent>
-        <Typography type="title" component="div">
-          ENS: {props.searchResult.searchName}.eth
-        </Typography>
-        { hidden ? null :
-          (
-            <Typography type="title" component="div">
-              <p>Finalize Auction On</p>
-              <div>{props.registratesAt.toString()}</div>
-              <div>{endsMomentFromNow.toString()}</div>
-            </Typography>
-          )
-        }
-        {/* <Typography type="title" component="div">
-          Email: {props.formResult.email}
-        </Typography> */}
-        <Typography type="title" component="div">
-          ETH: {props.formResult.ethBid}
-        </Typography>
-        <Typography type="title" component="div">
-          Secret: {props.formResult.secret}
-        </Typography>
-        <Typography type="title" component="div">
-          TxHash: <a target='_blank' href={txHashUrl}>{props.revealTXHash}</a>
-        </Typography>
-        <Typography type="title" component="div">
-          JSON: <pre>{JSON.parse(props.exportJson)}</pre>
-        </Typography>
-        <CardActions className="RevealAuctionInfo-button">
+    <Card raised>
+      <div className='RevealAuctionInfo'>
+        {finalizeAuctionOn}
+        {infoItems}
+        <div className='RevealAuctionInfo-actions'>
           <Button raised onClick={() => props.switchPage('main')}>BACK TO SEARCH</Button>
           <Button raised>MY ENS LIST</Button>
-        </CardActions>
-      </CardContent>
+        </div>
+      </div>
     </Card>
   );
 };
