d4c96e5a1450a9d84d51aa3907b4e9c67eccd8b2
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-11-09 23:33:36 +0800

    Update Menu, add Disclaimer, FAQ

--- src/components/FAQ.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const FAQ = () => (
-  <div>
-    <h2>FAQ</h2>
-    <div>FAQ description</div>
-  </div>
-);
---
+++ src/constants/menu.js
@@ -0,0 +1,9 @@
+export const ENS_BID = 'ENS_BID';
+export const MY_ENS_LIST = 'MY_ENS_LIST';
+export const ENS_TRADE = 'ENS_TRADE';
+export const ENS_LOAD = 'ENS_LOAD';
+
+export const FAQ = 'FAQ';
+export const DISCLAIMER = 'DISCLAIMER';
+export const BUG_REPORT = 'BUG_REPORT';
+export const SETTINGS = 'SETTINGS';
